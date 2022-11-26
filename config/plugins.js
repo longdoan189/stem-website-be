15dda23df56fdeb7d9fc8cd58f5d5a34bac06f97
Nguyen Long <long.doan.180021@student.fulbright.edu.vn>
2022-11-26 16:17:00 +0100

    import/export

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,17 @@
 module.exports = ({ env }) => ({
     // ...
+    'import-export-entries': {
+      enabled: true,
+      config: {
+        /**
+         * Public hostname of the server.
+         *
+         * If you use the local provider to persist medias,
+         * `serverPublicHostname` should be set to properly export media urls.
+         */
+        serverPublicHostname: 'http://localhost:1337/', // default: "".
+      },
+    },
     upload: {
       config: {
         provider: 'cloudinary',
